ef97bf8fdf51323479e78d5f0360190910ad5d63
Brieuc GUILLOU <guillou.brieuc@gmail.com>
2023-04-14 00:54:05 +0200

    feat: Dialog

--- src/app/pages/form-field-demo/form-field-demo.component.ts
+++ src/app/pages/form-field-demo/form-field-demo.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+
+export interface DialogData {
+  animal: 'panda' | 'unicorn' | 'lion';
+}
 
 @Component({
   selector: 'app-form-field-demo',
@@ -7,4 +12,29 @@ import { Component } from '@angular/core';
 })
 export class FormFieldDemoComponent {
   hide = true;
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(DialogDataExampleDialog, {
+      data: {
+        animal: 'panda',
+      },
+    });
+  }
+}
+
+@Component({
+  selector: 'dialog-data-example-dialog',
+  template: `<h1 mat-dialog-title>Favorite Animal</h1>
+    <div mat-dialog-content>
+      My favorite animal is:
+      <ul>
+        <li><span *ngIf="data.animal === 'panda'">&#10003;</span> Panda</li>
+        <li><span *ngIf="data.animal === 'unicorn'">&#10003;</span> Unicorn</li>
+        <li><span *ngIf="data.animal === 'lion'">&#10003;</span> Lion</li>
+      </ul>
+    </div>`,
+})
+export class DialogDataExampleDialog {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
 }
