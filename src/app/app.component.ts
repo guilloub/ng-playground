ff4ec38e0d4edec3620a086ff36504a4c953222e
Brieuc GUILLOU <guillou.brieuc@gmail.com>
2023-04-15 00:42:56 +0200

    fix: density toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,23 +10,42 @@ export class AppComponent {
   title = 'ng-playground';
 
   public darkModeUI = false;
+  public denseModeUI = false;
 
   constructor(private overlayContainer: OverlayContainer) {}
 
   @HostBinding('class')
-  public get themeMode() {
-    return this.darkModeUI ? 'dark-theme' : 'light-theme';
+  public get themeClass() {
+    const theme = this.darkModeUI ? 'dark-theme' : 'light-theme';
+    const density = this.denseModeUI ? 'high-density' : 'low-density';
+
+    return theme + ' ' + density;
   }
 
   public handleDarkThemeOn(isDark: boolean) {
     this.darkModeUI = isDark;
-    this.applyThemeToOverlyContainers();
+    this.applyThemeToOverlayContainers();
   }
 
-  private applyThemeToOverlyContainers() {
+  public handlHighDensityOn(isDense: boolean) {
+    this.denseModeUI = isDense;
+    this.applyThemeToOverlayContainers();
+  }
+
+  private applyThemeToOverlayContainers() {
     const overlayContainerClasses =
       this.overlayContainer.getContainerElement().classList;
-    overlayContainerClasses.remove('dark-theme', 'light-theme');
-    overlayContainerClasses.add(this.themeMode);
+
+    overlayContainerClasses.remove(
+      'dark-theme',
+      'light-theme',
+      'high-density',
+      'low-density'
+    );
+
+    const theme = this.darkModeUI ? 'dark-theme' : 'light-theme';
+    const density = this.denseModeUI ? 'high-density' : 'low-density';
+
+    overlayContainerClasses.add(theme, density);
   }
 }
